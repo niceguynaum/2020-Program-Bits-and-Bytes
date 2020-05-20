b0347b58bbfda60fc6fe71744c4dd00736d11301
Nuan Sonar <niceguynaum@gmail.com>
2020-05-20 17:39:56 -0500

    Fixed Up the Numbers to English Program
    The program is now complete; I refactored quite a lot of it and got rid of the needlessly complex vector pushback logic as well as conditional looping. Instead this approach words a lot more in chunks and iterates on previous code sequentially.

--- 2020.5/Number to English/Number to English/Number to English.cpp
+++ 2020.5/Number to English/Number to English/Number to English.cpp
@@ -8,62 +8,81 @@ supported by a 32-bit integer (approximately -2 billion to 2 billion).
 
 #include <iostream>
 #include <string.h>
-#include <vector>
-#include <array>
-#include <cmath>
 
 // first input: 1024
 
-std::string sNumberOut(int iNumberInput)
+std::string sEnglishOut;
+std::string sOneToTen[] = { "",
+	"one", "two", "three", "four", "five",
+	"six", "seven", "eight", "nine", "ten",
+	"eleven", "twelve", "thirteen", "fourteen",
+	"fifteen", "sixteen", "seventeen", "eighteen",
+	"nineteen" };
+
+std::string sTensToHundred[] = { "",
+	"twenty", "thirty", "fourty", "fifty",
+	"sixty", "seventy", "eighty", "ninety" };
+
+std::string sPowerOfTen[] = {
+	"", "thousand", "million", "billion" };
+
+std::string padIfNeeded(std::string output)
 {
-	int iTensToHundred = 0;
-	int iPowersOfTen = -1;
-	std::vector<int> vStringArrayintStore;
-	std::vector<std::string> vEnglishOut;
-	std::string sEnglishOut;
-
-	std::string sOneToTen[] = { "", 
-		"one", "two", "three", "four", "five",
-		"six", "seven", "eight", "nine", "ten",
-		"eleven", "twelve", "thirteen", "fourteen",
-		"fifteen", "sixteen", "seventeen", "eighteen",
-		"nineteen" };
-
-	std::string sTensToHundred[] = { "",
-		"twenty", "thirty", "fourty", "fifty", 
-		"sixty", "seventy", "eighty", "ninety"};
-
-	std::string sPowerOfTen[] = {
-		"", "hundred", "thousand", 
-		"hundred thousand", "million", "billion"};
-
-	for (int i = iNumberInput; i < 20;)
-	{
-		// a ternary operation that handles both power of ten size as well as 19 and below output.
-		(i > 19) ? vStringArrayintStore.push_back(i%10), i/10, iPowersOfTen++: (iPowersOfTen = -1) ? vEnglishOut.push_back(sOneToTen[i]), i: i;
-		if (i < 100 && i > 19) {
-
-		}
+	if (output == "") {
+		return "";
 	}
-
-	//logic loop for output assembly
-	for (int i = 0; i = vEnglishOut.size(); i++)
-	{
-
-		sEnglishOut = vEnglishOut[i] + " " + sEnglishOut;
+	return " " + output;
+}
+std::string translateHundred(int hundred_chunk) // Method to handle cases in the teens up to 100
+{
+	if (hundred_chunk < 20) {
+		return sOneToTen[hundred_chunk];
 	}
-
-	if (iNumberInput < 0) sEnglishOut = "negative " + sEnglishOut;
-	return sEnglishOut;
+	int tens = hundred_chunk / 10;
+	int ones = hundred_chunk % 10;
+	return sTensToHundred[tens] + padIfNeeded(sOneToTen[ones]);
 }
 
+std::string translateThousand(int thousand_chunk)
+{
+	if (thousand_chunk < 100) {
+		return translateHundred(thousand_chunk);
+	}
+	else {
+		int hundreds = thousand_chunk / 100;
+		int hundred_chunk = thousand_chunk % 100;
+		return sOneToTen[hundreds]
+			+ " hundred"
+			+ padIfNeeded(translateHundred(hundred_chunk));
+	}
+}
 
 int main()
 {
 	int iNumberInput = 0;
-	std::cout << "Please enter a number (2 billion is the max): " << std::endl;
-	//std::cin >> iNumberInput;
-	/*DEBUG*/ iNumberInput = 19;
-	printf("%s\n", sNumberOut(iNumberInput).c_str());
-	std::cout << iNumberInput;
+	std::string sNumber;
+	bool is_negative = false;
+	std::cout << "Please enter a positive or negative number (2 billion is the max): " << std::endl;
+	std::cin >> iNumberInput;
+	if (iNumberInput < 0) {
+		is_negative = true;
+		iNumberInput *= -1;
+	}
+	int chunk_count = 0;
+	while (iNumberInput > 0) {
+		if (iNumberInput % 1000 != 0) {
+			sNumber = translateThousand(iNumberInput % 1000)
+				+ padIfNeeded(sPowerOfTen[chunk_count]
+				+ padIfNeeded(sNumber));
+		}
+		iNumberInput /= 1000;
+		chunk_count++;
+	}
+	if (sNumber == "") {
+		sNumber = "zero";
+	}
+	if (is_negative) {
+		sNumber = "negative " + sNumber;
+	}
+	printf("%s\n", sNumber.c_str());
 }
